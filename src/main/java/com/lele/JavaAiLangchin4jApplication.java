058b8ba1cac1b781679c47b67f9699995d6c6ba0
LELE <3080875439@qq.com>
2026-02-04 22:55:49 +0800

    first commit

---
+++ src/main/java/com/lele/JavaAiLangchin4jApplication.java
@@ -0,0 +1,13 @@
+package com.lele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiLangchin4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAiLangchin4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lele/JavaAiLangchin4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiLangchin4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
