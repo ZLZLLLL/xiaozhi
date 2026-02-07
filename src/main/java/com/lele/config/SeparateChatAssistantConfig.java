698ccd727870b027ad5c6d4d7794eb12c0cbcb27
LELE <3080875439@qq.com>
2026-02-07 22:35:17 +0800

    没有更新

--- src/main/java/com/lele/config/SeparateChatAssistantConfig.java
+++ src/main/java/com/lele/config/SeparateChatAssistantConfig.java
@@ -2,6 +2,7 @@ package com.lele.config;
 
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,12 @@ public class SeparateChatAssistantConfig {
 
     @Bean
     public ChatMemoryProvider chatMemoryProvider() {
-        return memoryId -> MessageWindowChatMemory.builder().id(memoryId).maxMessages(10).build();
+
+        return memoryId -> MessageWindowChatMemory
+                .builder()
+                .id(memoryId)
+                .maxMessages(10)
+                .chatMemoryStore(new InMemoryChatMemoryStore())
+                .build();
     }
 }
