7ff1127f33f83f1dc159d4d492dcd4d752800be8
LELE <3080875439@qq.com>
2026-02-12 22:17:26 +0800

    使用mongoDB

--- src/main/java/com/lele/story/MongoChatMemoryStore.java
+++ src/main/java/com/lele/story/MongoChatMemoryStore.java
@@ -14,16 +14,31 @@ public class MongoChatMemoryStore implements ChatMemoryStore {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    /**
+     * 根据会话ID获取聊天消息
+     * @param o 会话ID
+     * @return 聊天消息列表
+     */
     @Override
     public List<ChatMessage> getMessages(Object o) {
         return List.of();
     }
+
+    /**
+     * 新增或更新聊天记录
+     * @param o 会话ID
+     * @param list 消息列表
+     */
 
     @Override
     public void updateMessages(Object o, List<ChatMessage> list) {
 
     }
 
+    /**
+     * 根据会话ID删除聊天消息
+     * @param o 会话ID
+     */
     @Override
     public void deleteMessages(Object o) {
 
